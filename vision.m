7779250f5bf189255c83559bf0547b1691a9c56e
ferinnj1 <ferinnj1@ITSD-GNTQLX1-WL.dom1.jhuapl.edu>
2017-07-18 15:24:10 -0400

    xzcfgntfxmmt

--- vision.m
+++ vision.m
@@ -10,14 +10,14 @@ camR.Resolution = '352x288';
 camL.Resolution = '352x288';
 
 q = 0;
-while(true)
+while(q == 0)
     picR = snapshot(camR);
     picL = snapshot(camL);
     r = calc(picR);
     l = calc(picL);
     q = q + 1;
     if r(1) == 1 && l(1) == 1
-       %coordinateConverter(r(2), r(3), l(2), l(3))
+       coordinateConverter(r(2), r(3), l(2), l(3))
     end
 end
 %%
