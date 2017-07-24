4dc674662ff0e3fe8df9e58c0c579dfc77177cba
engelja1 <joshuaengels@yahoo.com>
2017-07-24 10:50:01 -0400

    New Values

--- setGlobal.m
+++ setGlobal.m
@@ -2,8 +2,8 @@ function f = setGlobal()
 global initialXThreat initialZThreat initialVXThreat initialVZThreat;
 initialXThreat = -15;
 initialZThreat = 0;
-initialVXThreat = 5;
-initialVZThreat = 15;
+initialVXThreat = 10 * cos(pi / 3);
+initialVZThreat = 15 * sin(pi / 3);
 
 global initialXInterceptor initialZInterceptor initialVXInterceptor initialVZInterceptor;
 initialXInterceptor = 0;
@@ -16,11 +16,12 @@ global sigmaX sigmaZ Q;
 sigmaX = 0.4; %When you change this change Q as well. Actually not sure of this
 sigmaZ = 0.4;
 Q =  .01 * diag(ones(6, 1)); %.01 a good value for 0.5/0.4
+%Q =  .1 * (ones(6, 6)); %.01 a good value for 0.5/0.4
 
 %If estimated time until launch is less than this, stop camera measurments
 %and commence better estimates and launch
 global timeThreshhold;
-timeThreshhold = 0.3;
+timeThreshhold = -3.0;
 
 %Define parameters for threat structure
 global threatParams;
