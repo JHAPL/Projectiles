bb7cf7a36e1950dd4b40d655d3668af0638ddfa0
engelja1 <joshuaengels@yahoo.com>
2017-07-24 13:34:43 -0400

    Working on fixing kalman filter

--- setGlobal.m
+++ setGlobal.m
@@ -13,15 +13,15 @@ initialVZInterceptor = 15 * cos(pi/4);
 
 %Process error matrix
 global sigmaX sigmaZ Q;
-sigmaX = 0.4; %When you change this change Q as well. Actually not sure of this
-sigmaZ = 0.4;
-Q =  .01 * diag(ones(6, 1)); %.01 a good value for 0.5/0.4
+sigmaX = 0.1; %When you change this change Q as well. Actually not sure of this
+sigmaZ = 0.1;
+Q =  0.01 * diag(ones(6, 1)); %.01 a good value for 0.5/0.4
 %Q =  .1 * (ones(6, 6)); %.01 a good value for 0.5/0.4
 
 %If estimated time until launch is less than this, stop camera measurments
 %and commence better estimates and launch
 global timeThreshhold;
-timeThreshhold = -3.0;
+timeThreshhold = 0.5;
 
 %Define parameters for threat structure
 global threatParams;
