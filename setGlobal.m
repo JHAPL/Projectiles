bdd267a0ff3a4f51e19543673b29145e51e4958a
engelja1 <joshuaengels@yahoo.com>
2017-07-26 10:48:31 -0400

    Some nice cleaning up

--- setGlobal.m
+++ setGlobal.m
@@ -1,8 +1,8 @@
 function f = setGlobal()
 global initialXThreat initialZThreat initialVXThreat initialVZThreat;
-initialXThreat = -15;
+initialXThreat = -20;
 initialZThreat = 0;
-initialVXThreat = 10 * cos(pi / 3);
+initialVXThreat = 15 * cos(pi / 3);
 initialVZThreat = 15 * sin(pi / 3);
 
 global initialXInterceptor initialZInterceptor initialVXInterceptor initialVZInterceptor;
@@ -13,15 +13,15 @@ initialVZInterceptor = 15 * cos(pi/4);
 
 %Process error matrix
 global sigmaX sigmaZ Q;
-sigmaX = 0.1; %When you change this change Q as well. Actually not sure of this
-sigmaZ = 0.1;
-Q =  0.01 * diag(ones(6, 1)); %.01 a good value for 0.5/0.4
-%Q =  .1 * (ones(6, 6)); %.01 a good value for 0.5/0.4
+sigmaX = .1; %When you change this change Q as well. Actually not sure of this
+sigmaZ = .1;
+Q =  .001 * diag(ones(6, 1)); %.01 a good value for 0.5/0.4
+%Q =  100000 * (ones(6, 6)); %.01 a good value for 0.5/0.4
 
 %If estimated time until launch is less than this, stop camera measurments
 %and commence better estimates and launch
 global timeThreshhold;
-timeThreshhold = 0.5;
+timeThreshhold = 0.3;
 
 %Define parameters for threat structure
 global threatParams;
