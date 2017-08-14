3ad8c3ee69d7f87e64ee1c8742a8ff81c6e01a3d
Joshua Engels <joshuaengels@yahoo.com>
2017-08-13 21:45:42 -0400

    This should work, but I did do it with no testing

--- vision.m
+++ vision.m
@@ -12,41 +12,51 @@ camL.Resolution = '352x288';
 
 setup();
 global timeThreshhold;
-testingKalman = true;
 
-lastPicture = tic;
+tic
 first = true;
 while(true)
-    calculationTime = tic;
 
     picR = snapshot(camR);
     picL = snapshot(camL);
+    dt = toc;
+    tic;
     r = calc(picR);
     l = calc(picL);
     if r(1) == 1 && l(1) == 1
         pos = coordinateConverter(r(2), r(3), l(2), l(3))
         
+        %Pass to kalman filter
+        if(first)
+            estimate = kfilter(true, pos(1), pos(3), []);
+            first = false;
+        else
+            estimate = kfilter(false, pos(1), pos(3), dt);
+        end
         
-        if(testingKalman)
-            dt = toc(lastPicture);
-      
-            if(first)
-                estimate = kfilter(true, pos(1), pos(3), []);
-                first = false;
-            else
-                estimate = kfilter(false, pos(1), pos(3), dt);
-            end
+        %If ball is moving down, stop the kalman filter and estimate landing point of ball
+        endLoop = false;
+        theta = estimate.theta;
+        if(theta(5) < 0)
+        
+            %Time interval, next 5 seconds
+            timeStep = 0.01;
+            time = 0:timeStep:(5 - timeStep);
+            paths = getPaths(time, theta(1), theta(3), theta(2), theta(5));
+            threatPath = paths.threat;
             
-            if (estimate.time < timeThreshhold)
-                projectedTime = trajectorymodel(theta(1), theta(4), theta(2), theta(5), false);
-                if(projectedTime < 0 || projectedTime == Inf)
-                    disp("Invalid Time Estimate");
+            %Finds first predicted place where z coordinate is less than 0, prints out the x coordinate there, assuming camera is at 0
+            %And that the threat launches negative to positive
+            for i = 1:length(threatPath)
+                if(threatPath(4) < 0) 
+                    disp(threatPath(3));
+                    endLoop = true;
                     break;
                 end
-                triggerSolenoid(projectedTime - toc(calculationTime));
-                break;
             end
-            lastPicture = tic;
-        end
+            
+            if(endLoop)
+                break;
+            end    
     end
 end
