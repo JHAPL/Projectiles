3f735427b76ec34531a0b4a1ff698afae970a9c8
Joshua Engels <joshuaengels@yahoo.com>
2017-08-16 13:06:47 -0400

    I hope this works. Again, no real tests

--- vision.m
+++ vision.m
@@ -13,17 +13,24 @@ camL.Resolution = '352x288';
 setup();
 global timeThreshhold;
 
-tic
+pictureTimer = tic
 first = true;
+timeEstimate = Inf;
+threshhold = 0.3;
+numPictures = 0;
 while(true)
 
+    codeTimer = tic;
     picR = snapshot(camR);
     picL = snapshot(camL);
-    dt = toc;
-    tic;
+
     r = calc(picR);
     l = calc(picL);
     if r(1) == 1 && l(1) == 1
+        
+        dt = toc(pictureTime);
+        pictureTimer = tic;
+        
         pos = coordinateConverter(r(2), r(3), l(2), l(3))
         
         %Pass to kalman filter
@@ -34,27 +41,28 @@ while(true)
             estimate = kfilter(false, pos(1), pos(3), dt);
         end
         
-        %If ball is moving down, stop the kalman filter and estimate landing point of ball
-        endLoop = false;
-        theta = estimate.theta;
-        if(theta(5) < 0)
+        timeEsimate = estimate.time;
+        lastTheta = estimate.theta;
         
-            %Time interval, next 5 seconds
-            timeStep = 0.01;
-            time = 0:timeStep:(5 - timeStep);
-            paths = getPaths(time, theta(1), theta(3), theta(2), theta(5));
-            threatPath = paths.threat;
-            
-            %Finds first predicted place where z coordinate is less than 0, prints out the x coordinate there, assuming camera is at 0
-            %And that the threat launches negative to positive
-            for i = 1:length(threatPath)
-                if(threatPath(4) < 0) 
-                    disp(threatPath(3));
-                    break;
-                end
-            end
-            
-            break;
+        numPictures = numPictures + 1;
  
+    end
+    
+    codeTimeElapsed = toc(codeTimer);
+    timeEstimate = timeEstimate - codeTimeElapsed;
+    
+    if(timeEstimate < threshhold) %Can add a condition here like if numPictures > 4 or whatever to ensure its accurate
+        tic
+        projectedTime = trajectorymodel(lastTheta(1), lastTheta(4), lastTheta(2), lastTheta(5), false);
+        projectedTime = projectedTime - toc(pictureTime);
+
+                
+        if(projectedTime < 0 || projectedTime == Inf)
+            disp("Invalid Time Estimate");
+        end
+                
+        triggerSolenoid(projectedTime);
+
+        break;    
     end
 end
